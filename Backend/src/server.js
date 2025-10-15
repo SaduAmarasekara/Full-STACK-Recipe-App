044c6d17fec91bff5040ce4c11a1ca36ff5c3fed
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-10-15 14:28:29 +0530

    db added

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,8 +5,8 @@ const app = express()
 const PORT = ENV.PORT || 5001;
 
 app.get("/api/health", (req, res) => {
-    res.status(200).json({ success: true });
-});
+    res.status(200).json({ success: true})
+})
 
 app.listen(5001, () => {
     console.log("Server is running on PORT:", PORT);
